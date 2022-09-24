1dcddc1b0b27e7e747feb2981f6dae76e743aef2
Rafael Araujo <rflaraujodev@gmail.com>
2022-09-24 15:31:40 -0300

    feat: addressbar color primary

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
 </template>
 
 <script lang="ts">
+import { colors } from "quasar";
+const { getPaletteColor } = colors;
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
+  mounted() {
+    this.$q.addressbarColor.set(getPaletteColor("primary"));
+  },
 });
 </script>
--- src/quasar-user-options.ts
+++ src/quasar-user-options.ts
@@ -2,10 +2,11 @@ import "./styles/quasar.sass";
 import lang from "quasar/lang/pt.js"; // Quasar language pt, es, en-US, etc.
 import "@quasar/extras/material-icons/material-icons.css";
 import "@quasar/extras/material-symbols-outlined/material-symbols-outlined.css";
+import { AddressbarColor } from "quasar";
 
 // To be used on app.use(Quasar, { ... })
 export default {
   config: {},
-  plugins: {},
+  plugins: { AddressbarColor },
   lang: lang,
 };
